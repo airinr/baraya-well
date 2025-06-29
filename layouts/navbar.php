9a47458ead0c4724f380884465f252e966c057e4
Airin Ristiana <airinristianaaa@gmail.com>
2025-06-29 10:42:10 +0700

    first commit

---
+++ layouts/navbar.php
@@ -0,0 +1,22 @@
+<nav class="flex items-center justify-between px-8 py-4 shadow">
+  <!-- Logo -->
+  <div class="flex items-center space-x-2">
+    <img src="src/assets/images/logo-baraya-well.png" alt="Logo" class="h-8">
+    <h1 class="text-xl font-semibold">
+      <span class="text-blue-600">Baraya</span>
+      <span class="text-yellow-500 font-medium">Well</span>
+    </h1>
+  </div>
+
+  <!-- Navigation Menu -->
+  <ul class="flex space-x-8">
+    <li><a href="#" class="font-semibold text-black hover:text-blue-600">Home</a></li>
+    <li><a href="#" class="font-semibold text-black hover:text-blue-600">About Us</a></li>
+    <li><a href="#" class="font-semibold text-black hover:text-blue-600">Contact</a></li>
+  </ul>
+
+  <!-- Login Button -->
+  <div>
+    <a href="#" class="font-bold text-black hover:text-blue-600">Login</a>
+  </div>
+</nav>
